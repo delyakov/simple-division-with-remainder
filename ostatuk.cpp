6be1280d8af07e97212c2f47b639f605c7d3d57f
Dimitar Delyakov <41669335+delyakov@users.noreply.github.com>
2023-02-07 13:55:45 +0200

    Update ostatuk.cpp

--- ostatuk.cpp
+++ ostatuk.cpp
@@ -2,25 +2,24 @@
 using namespace std;
 int main()
 {
-	int x,y,z,i=0;
-	cout<<"Programata smqta delenie s ostatuk. Vuvedete dve celi chisla x,y suotvetno delimo i delitel. Programata izvejda ostatuka. Matematicheski vid na uravnenieto: x % y = z"<<endl<<endl;
-	while(i=1) {
+	int x,y,z;
+	cout<<"Programata smqta delenie s ostatuk. Vuvedete dve celi chisla x,y suotvetno delimo-delitel. Programata izvejda ostatuka. Matematicheski vid na uravnenieto: x % y = z"<<endl<<endl;
+	while (1) {
 		cout<<"Vuvedete x: ";
 		cin>>x;
 		cout<<"Vuvedete y: ";
 		cin>>y;
-		if(y==0) {
-			cout<<endl;
-			cout<<"Molq vuvedete razlichno chislo ot 0!"<<endl;
-			break;
+		if (y==0) {
+			cout<<"Molq vuvedete razlichno chislo ot 0!"<<endl<<endl;
+			continue;
 		} else {
 			z=x%y;
-			if(z<0) {
+			if (z<0) {
 				z=z*(-1);
 			}
 			cout<<"Ostatuk: "<<z<<endl<<endl;
 		}	
 	}
 	system("PAUSE");
-    return 0;
+	return 0;
 }
